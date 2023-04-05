f467d20eb73d16e1a36c2de2a70321bf35e89eb6
mounacqflo <mounacqflo@cy-tech.fr>
2023-04-05 16:58:53 +0200

    leaderboard works

--- Assets/FoxAdventures/Cours/Scripts/Leaderboard/UI/Leaderboard.cs
+++ Assets/FoxAdventures/Cours/Scripts/Leaderboard/UI/Leaderboard.cs
@@ -1,110 +1,79 @@
 using System.Collections.Generic;
+using PlayFab;
+using PlayFab.ClientModels;
 using UnityEngine;
 
 public class Leaderboard : MonoBehaviour
 {
     public string leaderboardName = "";
     public GameObject leaderboardEntryPrefab = null;
-    //
     private List<LeaderboardEntry> leaderboardEntries = new List<LeaderboardEntry>();
-    //
     public bool isFloatExpected = false;
     public bool isReversed = false;
 
-    // Start is called before the first frame update
     void OnEnable()
     {
-        // Deactivate prefab in case
         if (this.leaderboardEntryPrefab != null)
             this.leaderboardEntryPrefab.gameObject.SetActive(false);
-
-        // Refresh prefab
         this.RefreshLeaderboard();
     }
 
-    // Refresh
     public void RefreshLeaderboard()
     {
-        // Check prefab
         if (this.leaderboardEntryPrefab == null)
             return;
-
-        // Trigger leaderboard data retrieval
         int startPosition = 0;
         int maxEntries = 10;
+        PlayFabClientAPI.GetLeaderboard(new GetLeaderboardRequest
+        {
+            StartPosition = startPosition,
+            MaxResultsCount = maxEntries,
+            StatisticName = leaderboardName
+        }, OnGetLeaderboardSuccess, OnGetLeaderboardError);
     }
 
-    private void OnGetLeaderboardSuccess()
+    private void OnGetLeaderboardSuccess(GetLeaderboardResult result)
     {
-        // Clear existing entries
         this.ClearExistingEntries();
-
-        //// TODO: Use data from input & loop on it?
-        // Go through scores
-        //if (data)
+        foreach (var dataEntry in result.Leaderboard)
         {
-            //for (int i=0; i < data.Leaderboard.Count; i++)
-            {
-                //// Use data from input & loop on it?
-                //if (dataEntry)
+            string username = dataEntry.DisplayName;
+            int statValue = dataEntry.StatValue;
+            GameObject leaderboardEntryGameobjectCopy = GameObject.Instantiate(this.leaderboardEntryPrefab, this.leaderboardEntryPrefab.transform.parent);
+            if (leaderboardEntryGameobjectCopy != null) {
+                leaderboardEntryGameobjectCopy.gameObject.SetActive(true);
+                LeaderboardEntry leaderboardEntry = leaderboardEntryGameobjectCopy.GetComponent<LeaderboardEntry>();
+                if (leaderboardEntry != null)
                 {
-                    // Get data
-                    string username = string.Empty;                         // TODO: Get user leaderboard name
-                    int statValue = 0;                                      // Get score we want to display
-
-                    // Instantiate object copy
-                    GameObject leaderboardEntryGameobjectCopy = GameObject.Instantiate(this.leaderboardEntryPrefab, this.leaderboardEntryPrefab.transform.parent);
-                    if (leaderboardEntryGameobjectCopy != null)
-                    {
-                        // Activate at our prefab is deactivated
-                        leaderboardEntryGameobjectCopy.gameObject.SetActive(true);
-
-                        // Get leaderboard entry
-                        LeaderboardEntry leaderboardEntry = leaderboardEntryGameobjectCopy.GetComponent<LeaderboardEntry>();
-                        if (leaderboardEntry != null)
-                        {
-                            // Fix value
-                            if (isReversed == true)
-                                statValue *= -1;
-
-                            // Set value
-                            leaderboardEntry.SetValue(username, (isFloatExpected == true ? ((float)statValue / 100.0f).ToString("0.00") : statValue.ToString()));
-
-                            // Add to list
-                            if (this.leaderboardEntries == null)
-                                this.leaderboardEntries = new List<LeaderboardEntry>();
-                            this.leaderboardEntries.Add(leaderboardEntry);
-                        }
-                        // Else, destroy object we just spawned
-                        else
-                        {
-                            GameObject.Destroy(leaderboardEntryGameobjectCopy);
-                        }
-                    }
+                    if (isReversed == true)
+                        statValue *= -1;
+                    leaderboardEntry.SetValue(username, (isFloatExpected == true ? ((float)statValue / 100.0f).ToString("0.00") : statValue.ToString()));
+                    if (this.leaderboardEntries == null)
+                        this.leaderboardEntries = new List<LeaderboardEntry>();
+                    this.leaderboardEntries.Add(leaderboardEntry);
+                }
+                else
+                {
+                    GameObject.Destroy(leaderboardEntryGameobjectCopy);
                 }
             }
         }
     }
 
-    private void OnGetLeaderboardError()
+    private void OnGetLeaderboardError(PlayFabError error)
     {
-        // Log
-        Debug.LogError("Leaderboard.OnGetLeaderboardError() - Error: TODO");
+        Debug.LogError($"Leaderboard.OnGetLeaderboardError() - Error: {error.ErrorMessage}");
     }
 
-    // Clear existing entries
     public void ClearExistingEntries()
     {
-        if (this.leaderboardEntries != null)
-        {
+        if (this.leaderboardEntries != null) {
             while (this.leaderboardEntries.Count > 0)
             {
                 if (this.leaderboardEntries[0] != null)
                 {
                     GameObject.Destroy(this.leaderboardEntries[0].gameObject);
                 }
-
-                // Remove first entry
                 this.leaderboardEntries.RemoveAt(0);
             }
         }
